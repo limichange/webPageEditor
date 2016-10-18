e246c60e30d6035c0e072ac7f0767ca6c2959c84
limichange <limichange@hotmail.com>
2016-10-18 14:08:04 +0800

    拆分区域

---
+++ src/components/MainEditArea.vue
@@ -0,0 +1,25 @@
+<template>
+<el-row>
+  <el-col :span="24">
+    <div class="grid-content bg-purple-dark">内容</div>
+  </el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+  name: 'main-edit-area',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,34 @@
+<template>
+<el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal" @select="handleselect">
+  <el-menu-item index="1">Design</el-menu-item>
+<!--   <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+  </el-submenu> -->
+  <el-menu-item index="2">Prototype</el-menu-item>
+</el-menu>
+</template>
+
+<script>
+export default {
+  name: 'main-nav',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    handleselect (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-menu {
+    border-radius: 0;
+  }
+</style>
