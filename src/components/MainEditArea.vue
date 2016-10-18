3953256c0ca0e720839dee570de9ea460bfd46b5
limichange <limichange@hotmail.com>
2016-10-18 18:05:07 +0800

    update

--- src/components/MainEditArea.vue
+++ src/components/MainEditArea.vue
@@ -10,18 +10,21 @@
     </el-row>
   </el-col>
   <el-col :span="20" >
-    <div class="webpage-c">
-      <div class="webpage">
-        <div class="cube"></div>
-      </div>
-    </div>
+    <webpage>
+      <div class="cube"></div>
+    </webpage>
   </el-col>
 </el-row>
 </template>
 
 <script>
+import Webpage from './WebPage.vue'
+
 export default {
   name: 'main-edit-area',
+  components: {
+    Webpage
+  },
   data () {
     return {
       value2: 64
@@ -43,6 +46,12 @@ export default {
 
 <style lang="scss" scoped>
 
+.cube {
+  width: 5rem;
+  height: 5rem;
+  background: #eee;
+}
+
 .slideBar {
   background: #cccccc;
 }
@@ -50,25 +59,6 @@ export default {
 .main-edit-area {
   background: #e5e9f2;
   height: 100%;
-}
-
-.webpage-c {
-  margin: 0 auto;
-  width: 10rem;
-  height: 16.1rem;
-
-  .webpage {
-
-    .cube {
-      width: 5rem;
-      height: 5rem;
-      background: #eee;
-    }
-
-    width: 10rem;
-    height: 16.1rem;
-    background: #13CE66;
-  }
 }
 
 </style>
---
+++ src/components/WebPage.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="webpage-c">
+  <div class="webpage">
+    <slot></slot>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'webpage',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.webpage-c {
+  margin: 0 auto;
+  width: 10rem;
+  height: 16.1rem;
+
+  .webpage {
+    width: 10rem;
+    height: 16.1rem;
+    background: #13CE66;
+  }
+}
+</style>
