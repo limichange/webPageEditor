59ec9c67b452561dc260eeb8e1279e0f65396069
limichange <limichange@hotmail.com>
2016-10-18 16:20:32 +0800

    update

--- src/components/MainEditArea.vue
+++ src/components/MainEditArea.vue
@@ -1,7 +1,16 @@
 <template>
-<el-row>
-  <el-col :span="24">
-    <div class="grid-content bg-purple-dark">内容</div>
+<el-row class="main-edit-area">
+  <el-col :span="4" >
+    <el-row>
+      <el-col :span="24">
+        <el-button>按钮1</el-button>
+      </el-col>
+    </el-row>
+  </el-col>
+  <el-col :span="20" >
+    <div class="webpage-c">
+      <div class="webpage"></div>
+    </div>
   </el-col>
 </el-row>
 </template>
@@ -20,6 +29,23 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.main-edit-area {
+  background: #e5e9f2;
+  height: 100%;
+}
+
+.webpage-c {
+  margin: 0 auto;
+  width: 640px;
+  height: 1030px;
+
+  .webpage {
+    width: (640 / 2)px;
+    height: (1030 / 2)px;
+    background: #13CE66;
+  }
+}
 
 </style>
